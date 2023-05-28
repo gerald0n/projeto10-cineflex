d9756729e64b8a3864a9a0ad510193d4431ef564
Geraldo Gomes <contato.gneto@gmail.com>
2023-05-28 03:05:56 -0300

    add new feature: homepage with movie list

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import GlobalStyle from './style/GlobalStyle.js'
+import ResetStyle from './style/ResetStyle.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+   <React.StrictMode>
+      <GlobalStyle />
+      <ResetStyle />
+      
+      <App />
+   </React.StrictMode>
 )
--- src/pages/SessionsPage/SessionsPage.jsx
+++ src/pages/SessionsPage/SessionsPage.jsx
@@ -1,118 +1,121 @@
-import styled from "styled-components"
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import styled from 'styled-components'
 
-export default function SessionsPage() {
+export default function SessionsPage({ movieID }) {
+   const movie_URL = `https://mock-api.driven.com.br/api/v8/cineflex/movies/${movieID}/showtimes`
+   const [movie, setMovie] = useState([])
 
-    return (
-        <PageContainer>
-            Selecione o horário
+   useEffect(() => {
+      axios.get(movie_URL).then((response) => {
+         setMovie(response.data)
+      })
+   }, [])
+
+   const { days: arrayOfAvailableDays } = movie
+
+   return (
+      <PageContainer>
+         Selecione o horário
+         <div>
+            {arrayOfAvailableDays && arrayOfAvailableDays.map((day) => {
+               const { id, weekday, date, showtimes } = day
+               /* console.log(day) */
+               return (
+                  <SessionContainer key={id}>
+                     {weekday} - {date}
+                     <ButtonsContainer>
+                        {showtimes.map(time => (<button key={time.id}>{time.name}</button>))}
+                     </ButtonsContainer>
+                  </SessionContainer>
+               )
+            })}
+         </div>
+         <FooterContainer>
             <div>
-                <SessionContainer>
-                    Sexta - 03/03/2023
-                    <ButtonsContainer>
-                        <button>14:00</button>
-                        <button>15:00</button>
-                    </ButtonsContainer>
-                </SessionContainer>
-
-                <SessionContainer>
-                    Sexta - 03/03/2023
-                    <ButtonsContainer>
-                        <button>14:00</button>
-                        <button>15:00</button>
-                    </ButtonsContainer>
-                </SessionContainer>
-
-                <SessionContainer>
-                    Sexta - 03/03/2023
-                    <ButtonsContainer>
-                        <button>14:00</button>
-                        <button>15:00</button>
-                    </ButtonsContainer>
-                </SessionContainer>
+               <img
+                  src={movie.posterURL}
+                  alt="poster"
+               />
             </div>
-
-            <FooterContainer>
-                <div>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster" />
-                </div>
-                <div>
-                    <p>Tudo em todo lugar ao mesmo tempo</p>
-                </div>
-            </FooterContainer>
-
-        </PageContainer>
-    )
+            <div>
+               <p>{movie.title}</p>
+            </div>
+         </FooterContainer>
+      </PageContainer>
+   )
 }
 
 const PageContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    font-family: 'Roboto';
-    font-size: 24px;
-    text-align: center;
-    color: #293845;
-    margin-top: 30px;
-    padding-bottom: 120px;
-    padding-top: 70px;
-    div {
-        margin-top: 20px;
-    }
+   display: flex;
+   flex-direction: column;
+   font-family: 'Roboto';
+   font-size: 24px;
+   text-align: center;
+   color: #293845;
+   margin-top: 30px;
+   padding-bottom: 120px;
+   padding-top: 70px;
+   div {
+      margin-top: 20px;
+   }
 `
 const SessionContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    font-family: 'Roboto';
-    font-size: 20px;
-    color: #293845;
-    padding: 0 20px;
+   display: flex;
+   flex-direction: column;
+   align-items: flex-start;
+   font-family: 'Roboto';
+   font-size: 20px;
+   color: #293845;
+   padding: 0 20px;
 `
 const ButtonsContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    margin: 20px 0;
-    button {
-        margin-right: 20px;
-    }
-    a {
-        text-decoration: none;
-    }
+   display: flex;
+   flex-direction: row;
+   margin: 20px 0;
+   button {
+      margin-right: 20px;
+   }
+   a {
+      text-decoration: none;
+   }
 `
 const FooterContainer = styled.div`
-    width: 100%;
-    height: 120px;
-    background-color: #C3CFD9;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    font-size: 20px;
-    position: fixed;
-    bottom: 0;
+   width: 100%;
+   height: 120px;
+   background-color: #c3cfd9;
+   display: flex;
+   flex-direction: row;
+   align-items: center;
+   font-size: 20px;
+   position: fixed;
+   bottom: 0;
 
-    div:nth-child(1) {
-        box-shadow: 0px 2px 4px 2px #0000001A;
-        border-radius: 3px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background-color: white;
-        margin: 12px;
-        img {
-            width: 50px;
-            height: 70px;
-            padding: 8px;
-        }
-    }
+   div:nth-child(1) {
+      box-shadow: 0px 2px 4px 2px #0000001a;
+      border-radius: 3px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: white;
+      margin: 12px;
+      img {
+         width: 50px;
+         height: 70px;
+         padding: 8px;
+      }
+   }
 
-    div:nth-child(2) {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        p {
-            text-align: left;
-            &:nth-child(2) {
-                margin-top: 10px;
-            }
-        }
-    }
+   div:nth-child(2) {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      p {
+         text-align: left;
+         &:nth-child(2) {
+            margin-top: 10px;
+         }
+      }
+   }
 `
